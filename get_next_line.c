b3cd87e97d6181ee8a6a48c4dd76fce63000a3fa
Patarion <patarion.thelegend@gmail.com>
2022-01-06 09:35:05 -0500

    Fonctionnel sauf si nb_line > que dans fd

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line_utils.c"
 
-// #define BUFFER_SIZE 10
+ int BUFFER_SIZE = 10;
 
 char *get_next_line(int fd)
 {
@@ -13,8 +13,7 @@ char *get_next_line(int fd)
   while (r_read > 0)
   {
     lecture = (char *)malloc(sizeof(char *) * (BUFFER_SIZE + 1));
-    r_read = read(fd, lecture, 0);
-    if (r_read == -1)
+    if (read(fd, lecture, 0) == -1)
       return (0);
     r_read = read(fd, lecture, BUFFER_SIZE);
     buffer = ft_strconcatenate(buffer, lecture);
@@ -26,7 +25,7 @@ char *get_next_line(int fd)
   buffer = ft_substr(buffer, ft_findreturn(buffer) + 1);
   return (r_ligne);
 }
-/*
+
 int main (void)
 {
   int   fd;
@@ -35,10 +34,9 @@ int main (void)
 
   i = -1;
   fd = open("File.txt", O_RDONLY);
-  while (++i < 4)
+  while (++i < 5)
   {
     r_txt = get_next_line(fd);
     printf("%s", r_txt);
   }
 }
-*/
