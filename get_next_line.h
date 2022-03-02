3d41142c4aea3666798e03781204775fba17d4ac
Patarion <62268532+Patarion@users.noreply.github.com>
2022-03-02 12:19:00 -0500

    Gnl_2xnl_issue

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgagnon <marvin@42quebec.com>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 11:11:34 by jgagnon           #+#    #+#             */
-/*   Updated: 2022/01/10 11:11:44 by jgagnon          ###   ########.fr       */
+/*   Updated: 2022/01/30 17:00:13 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@
 # include <fcntl.h>
 
 char  *get_next_line(int fd);
-char  *ft_bufdup(const char *s, int end);
+char  *ft_bufdup(const char *s);
 int ft_strlen(const char * str);
 char	*ft_strconcatenate(const char *s1, const char *s2);
-char	*ft_subbuf(const char *s, int start);
+char	*ft_subbuf(const char *s);
 int ft_findreturn(const char *s);
 char  *ft_bufbeg(const char *s, int end);
 void *ini_mem(int size);
