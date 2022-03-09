c38bd0b9b82a27784596942ada2206e0a1655b60
Patarion <62268532+Patarion@users.noreply.github.com>
2022-03-09 11:37:37 -0500

    Gnl-V2_TO-BF1

--- get_next_line.h
+++ get_next_line.h
@@ -3,13 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 11:11:34 by jgagnon           #+#    #+#             */
-/*   Updated: 2022/01/30 17:00:13 by marvin           ###   ########.fr       */
+/*   Updated: 2022/03/08 16:34:01 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
@@ -21,13 +20,13 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-char  *get_next_line(int fd);
-char  *ft_bufdup(const char *s);
-int ft_strlen(const char * str);
+char	*get_next_line(int fd);
+char	*ft_bufdup(const char *s);
+int		ft_strlen(const char *str);
 char	*ft_strconcatenate(const char *s1, const char *s2);
 char	*ft_subbuf(const char *s);
-int ft_findreturn(const char *s);
-char  *ft_bufbeg(const char *s, int end);
-void *ini_mem(int size);
+int		ft_findreturn(const char *s);
+char	*ft_bufbeg(const char *s, int end);
+char	*ft_lecture(int fd, char *buffer);
 
 #endif
