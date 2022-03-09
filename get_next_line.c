2aec69c5f20aedf89ae9213fb9377f447052f951
Patarion <62268532+Patarion@users.noreply.github.com>
2022-03-09 11:36:45 -0500

    Gnl-V2_TO-BF1

--- get_next_line.c
+++ get_next_line.c
@@ -6,67 +6,108 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 11:17:33 by jgagnon           #+#    #+#             */
-/*   Updated: 2022/03/02 17:03:35 by coder            ###   ########.fr       */
+/*   Updated: 2022/03/08 18:16:13 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int   BUFFER_SIZE = 10;
-
-char *get_next_line(int fd)
+char	*ft_lecture(int fd, char *buffer)
 {
-  int         r_read;
-  char        *lecture;
-  char        *r_ligne;
-  static char *buffer;
+	int		r_read;
+	char	*lecture;
 
-  r_read = 1;
-  lecture = malloc(sizeof(char*) * (BUFFER_SIZE + 1));
-  if (!lecture)
-  {
-    free(lecture);
-    return (0);
-  }
-  if (fd < 0 || read(fd, lecture, 0) < 0 || BUFFER_SIZE < 1)
+	r_read = 1;
+	lecture = malloc(sizeof(char *) * (BUFFER_SIZE + 1));
+	if (!lecture)
 	{
 		free(lecture);
 		return (0);
 	}
-  while (r_read > 0 && ft_findreturn(buffer) == 0)
-  {
-    r_read = read(fd,lecture, BUFFER_SIZE);
-    lecture[r_read] = '\0';
-    buffer = ft_strconcatenate(buffer, lecture);
-  }
+	while (r_read > 0 && ft_findreturn(buffer) == 0)
+	{
+		r_read = read(fd, lecture, BUFFER_SIZE);
+		lecture[r_read] = '\0';
+		buffer = ft_strconcatenate(buffer, lecture);
+	}
 	free(lecture);
-  	if (ft_findreturn(buffer) > 0)
-	  {
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*r_ligne;
+	static char	*buffer;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, buffer, 0) < 0)
+		return (0);
+	buffer = ft_lecture(fd, buffer);
+	if (ft_findreturn(buffer) > 0)
+	{
 		r_ligne = ft_bufdup(buffer);
 		buffer = ft_subbuf(buffer);
 		return (r_ligne);
-	  }
+	}
 	else
 	{
 		r_ligne = ft_bufdup(buffer);
 		free(buffer);
 		buffer = NULL;
-		return(r_ligne);
+		return (r_ligne);
 	}
-  return (0);
+	return (0);
 }
 
-int main (void)
+char	*ft_bufdup(const char *s)
 {
-  int   fd;
-  char  *r_txt;
-  int    i;
+	int		j;
+	int		len;
+	char	*ptr;
 
-  i = -1;
-  fd = open("Texte.txt", O_RDONLY);
-  while (++i < 8)
-  {
-    r_txt = get_next_line(fd);
-//    printf("%s", r_txt);
-  }
+	j = 0;
+	len = 0;
+	while (s[len] != '\0' && s[len] != '\n')
+		len++;
+	ptr = (char *)malloc(sizeof(*ptr) * (len + 2));
+	if (!ptr || s[0] == '\0')
+	{
+		free(ptr);
+		return (0);
+	}
+	while (j < len + 1)
+	{
+		ptr[j] = s[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
+
+char	*ft_subbuf(const char *s)
+{
+	char	*ptr;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] != '\0' && s[i] != '\n')
+		i++;
+	j = ft_strlen(s) - i;
+	ptr = (char *)malloc(sizeof(char *) * (j + 1));
+	if (!ptr)
+	{
+		free(ptr);
+		return (0);
+	}
+	j = 0;
+	while (s[i + j] != '\0')
+	{
+		ptr[j] = s[i + 1 + j];
+		j++;
+	}
+	ptr[j] = '\0';
+	free((char *)s);
+	return (ptr);
 }
